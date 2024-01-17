f252e06c45d9eb4dc35d7bb96f18d1870095cada
attaullah khan <attaullahkhanit@gmail.com>
2024-01-17 18:58:10 +0500

    first-commit

---
+++ src/page/HomePage/Home.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles.scss';
+import { Typography } from '@mui/material';
+
+function Home() {
+  return (
+    <>
+        <div className='top-nav'>
+            <Typography>Let's begin the project from start</Typography>
+        </div>
+    </>
+  )
+}
+
+export default Home
