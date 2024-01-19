ac57d1ada7bdb98a8d4bf9031594a39dbf42a1ba
attaullah khan <attaullahkhanit@gmail.com>
2024-01-19 16:28:14 +0500

    second-commit

--- src/page/HomePage/Home.js
+++ src/page/HomePage/Home.js
@@ -3,6 +3,9 @@ import './styles.scss';
 import { Typography } from '@mui/material';
 
 function Home() {
+    const newarray = [1,2,3,4,5,6,7,4,3,3,3,5]
+    const uniqueSet = new Set(newarray)
+     console.log(uniqueSet, 'my newarray value')
   return (
     <>
         <div className='top-nav'>
